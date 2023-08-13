afe2acd84da634fa4548ca603e6a0b68e1aafa27
Iagho Slavieiro <71581382+iagho-slaviero@users.noreply.github.com>
2023-08-13 16:47:18 -0300

    feat: implementação strcpy

--- Library/string.h
+++ Library/string.h
@@ -6,3 +6,13 @@ int strlen(char *string){
 	return i;	
 }
 
+void strcpy(char *string, char *string2){
+	int i=0;
+	while(string2[i] != '\0'){
+		string[i] = string2[i];
+		i++;
+	}
+	string[i] = '\0';
+	
+	return string;
+}
