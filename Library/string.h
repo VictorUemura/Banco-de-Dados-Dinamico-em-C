fd77bca73c6959b9ea0ca9ec1bf6b97f6a5afbc5
VictorHUemura <victoruemura04@hotmail.com>
2023-08-13 15:14:54 -0300

    feat: Inserção struct para String dinâmica

--- Library/string.h
+++ Library/string.h
@@ -1,1 +1,20 @@
 
+// Implementação -> String dinâmica
+typedef struct Character {
+	struct Character *prox;
+	struct Character *ant;
+	char *C;
+} Character;
+
+typedef struct String {
+	int tam;
+	Character *inicio;
+	Character *fim;
+} String;
+
+void inicString(String *string) {
+	string->inicio = NULL;
+	string->fim = NULL;
+	string->tam = 0;
+}
+
