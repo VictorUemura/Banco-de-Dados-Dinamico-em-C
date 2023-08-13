5bb8ebfbd52bb10d1c8156e5c957be5798f79dda
VictorHUemura <victoruemura04@hotmail.com>
2023-08-13 16:56:19 -0300

    feat: strcmp para comparação de string

--- Library/string.h
+++ Library/string.h
@@ -1,3 +1,4 @@
+
 int strlen(char *string){
 	int i=0;
 	while(string[i] != '\0')
@@ -6,3 +7,14 @@ int strlen(char *string){
 	return i;	
 }
 
+char strcmp(char *string1, char *string2) {
+	int i = 0;
+	while(string1[i] != '\0' && string2[i] != '\0' && string1[i] == string2[i])
+		i++;
+	if(string1[i] < string2[i] || (string1[i] == '\0' && string2[i] != '\0'))
+		return -1;
+	else if(string1[i] == string2[i])
+		return 0;
+	else
+		return 1;
+}
