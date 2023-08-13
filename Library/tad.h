8914a4722be69107d3254e5f41a192e06f3dd1f2
VictorHUemura <victoruemura04@hotmail.com>
2023-08-13 00:34:26 -0300

    feat: Estrutura de arquivos e TAD

---
+++ Library/tad.h
@@ -0,0 +1,33 @@
+
+typedef struct Dado {
+	union {
+		int valorI;
+		float valorN;
+		char valorD[11];
+		char valorC;
+		char valorT[20];
+	};
+	struct Dado *prox;
+} Dado;
+
+typedef struct PColuna {
+	char tipo;
+	char *campo;
+	char pk;
+	Dado *pDados;
+	Dado *fk;	
+	struct PColuna *prox;
+} PColuna;
+
+typedef struct Tabela {
+	char *nome;
+	PColuna *coluna;
+	struct Tabela *ant;
+	struct Tabela *prox;
+} Tabela;
+
+typedef struct BancoDado {
+	char *nome;
+	Tabela *tabela;
+} BancoDado;
+
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "./library/date.h"
+#include "./library/file.h"
+#include "./library/interface.h"
+#include "./library/number.h"
+#include "./library/read.h"
+#include "./library/sql.h"
+#include "./library/string.h"
+#include "./library/tad.h"
+
+
+int main(void) {
+	
+	return 0;
+}
