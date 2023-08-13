85436dedf764604aa699e682602599d1f4e7445d
VictorHUemura <victoruemura04@hotmail.com>
2023-08-13 15:37:18 -0300

    feat: addCaractere no final da string

--- Library/string.h
+++ Library/string.h
@@ -24,3 +24,18 @@ Caractere *novoCaractere(char carac) {
 	c->C = carac;
 }
 
+// Adiciona caractere no final da string
+void addCaractere(String **string, char carac) {
+	Caractere *c = novoCaractere(carac);
+	if(*string->inicio == NULL) {
+		*string->inicio = *string->fim = c;
+	}
+	else {
+		c->ant = *string->fim;
+		*string->fim->prox = c;
+		*string->fim = c;
+	}
+	*string->tam++;
+}
+
+
