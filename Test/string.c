e2ef81276bfb85b95c760663f92a35b585f9f57a
VictorHUemura <victoruemura04@hotmail.com>
2023-08-13 16:34:38 -0300

    chore: Utilizar ponteiro de char

--- Test/string.c
+++ Test/string.c
@@ -1,12 +1,9 @@
 // Arquivo de teste biblioteca string
 #include<stdio.h>
+#include<stdlib.h>
 #include "../library/string.h"
 
 int main(void) {
-	String *string;
-	printf("fff");
-	inicString(&string);
-	addCaractere(&string, 'C');
-	exibeString(string);
+
 	return 0;
 }
