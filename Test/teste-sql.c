f7ce2ff51c672a38b62a08367b2e8650832c60e6
VictorHUemura <victoruemura04@hotmail.com>
2023-09-23 19:16:34 -0300

    feat: Criação de arquivo de biblioteca e teste sql

---
+++ Test/teste-sql.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "../Library/tad.h"
+#include "../Library/fila.h"
+#include "../Library/leituraArquivo.h"
+#include "../Library/sql.h"
+
+int main(void) {
+	return 0;
+}
